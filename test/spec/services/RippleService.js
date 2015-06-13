ab35d6c8ba59c1a5e1f8382fa28c853961db503f
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2015-06-13 13:07:56 +0200

    good test

--- test/spec/services/RippleService.js
+++ test/spec/services/RippleService.js
@@ -1,15 +1,13 @@
-var assert = require('chai');
+var assert = require('chai').assert;
 
 describe('RippleService', function () {
-
     var number = 43;
     var RippleService = require('../../../src/services/RippleService');
     beforeEach(function () {
-
         number = 17;
     });
 
-    it('should foo', function () {
+    it.only('should foo', function () {
         assert.equal(42, number);
     });
 });
