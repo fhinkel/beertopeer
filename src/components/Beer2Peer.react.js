9bc20b5d2bd49eff03ba95ec0e9fe78902f2b707
Martin Kreidenweis <martin.kreidenweis@tngtech.com>
2015-06-13 11:50:12 +0200

    added dependencies to ripple and creating connection during app startup
    LocalConfig.js can be used to set the ripple account/secret until we
    have user management
    please use LocalConfig.js.template as a template

--- src/components/Beer2Peer.react.js
+++ src/components/Beer2Peer.react.js
@@ -6,19 +6,44 @@ var {Route, RouteHandler, Link } = Router;
 var Styles = require('material-ui').Styles;
 var ThemeManager = new Styles.ThemeManager();
 var AppCanvas = require('material-ui').AppCanvas;
+var Remote = require('ripple-lib').Remote;
+
+var Config = require('../constants/Config');
+var LocalConfig = require('../constants/LocalConfig');
 
 var Header = require('./Header.react');
 
 var Beer2Peer = React.createClass({
 
-    //For Material UI
-    childContextTypes: {
-        muiTheme: React.PropTypes.object
-    },
+  //For Material UI
+  childContextTypes: {
+      muiTheme: React.PropTypes.object
+  },
 
-    getChildContext() {
-        return {muiTheme: ThemeManager.getCurrentTheme()};
-    },
+  getChildContext() {
+      return {muiTheme: ThemeManager.getCurrentTheme()};
+  },
+
+  componentDidMount() {
+    var remote = this.remote = new Remote(Config.rippleOptions);
+
+    remote.on('error', function (error) {
+      console.log('remote error: ', error);
+    });
+
+    remote.connect(function (err, res) {
+      if (err) {
+        console.log("error connecting", err);
+        return;
+      }
+
+    });
+  },
+
+  onLoggedInUserChanged() {
+    // TODO: access user store here
+    this.remote.setSecret(LocalConfig.ripple.account, LocalConfig.ripple.secret);
+  },
 
   render: function() {
     return (
