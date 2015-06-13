9d8b369003eff3013614fd73a88ce211f5ca20eb
Konstantin Knauf <konstantin.knauf@tngtech.com>
2015-06-13 15:44:05 +0200

    Join now validates existence of event code

--- src/components/Join.react.js
+++ src/components/Join.react.js
@@ -7,8 +7,10 @@ var React = require('react');
 var TextField = require('material-ui').TextField;
 var RaisedButton = require('material-ui').RaisedButton;
 
-var MIN_NUMBER_OF_CHARACTERS = 4;
-var MAX_NUMBER_OF_CHARACTERS = 6;
+var $ = require('jquery');
+
+var MIN_NUMBER_OF_CHARACTERS = 5;
+var MAX_NUMBER_OF_CHARACTERS = 5;
 
 var Join = React.createClass({
 
@@ -16,23 +18,39 @@ var Join = React.createClass({
         return {errorText: '' };
     },
 
-    checkCode(eventCode) {
+    getEventCode() {
+      return this.refs.eventCode.getValue().toString().toUpperCase();
+    },
+
+    checkCodeFormat(eventCode) {
         console.log('Checking '+ eventCode);
         return eventCode.toString().match(new RegExp('^[A-Z]{'+MIN_NUMBER_OF_CHARACTERS+','+MAX_NUMBER_OF_CHARACTERS+'}$'));
     },
 
-    onKeyDown() {
-        var eventCode = this.refs.eventCode.getValue().toString().toUpperCase();
-        if (!this.checkCode(eventCode)) {
+    codeExists(eventCode) {
+        $.get('http://46.101.128.85:3000/event/'+eventCode, function(data,status,xhr) {
+             return (status === 'success');
+        });
+    },
+
+    onKeyUp() {
+        var eventCode = this.getEventCode();
+        if (!this.checkCodeFormat(eventCode)) {
             this.setState( {errorText: MIN_NUMBER_OF_CHARACTERS+'-'+MAX_NUMBER_OF_CHARACTERS+' digits required.'});
+        } else if (!this.codeExists(eventCode)) {
+            if (this.getEventCode() === eventCode) {
+                this.setState( {errorText: "No event with this code exists."});
+            } else {
+                this.setState( {errorText: ''});
+            }
         } else {
             this.setState( {errorText: ''});
         }
     },
 
     joinEvent() {
-        var eventCode = this.refs.eventCode.getValue().toString().toUpperCase();
-        if (this.checkCode(eventCode)) {
+        var eventCode = this.getEventCode();
+        if (this.checkCodeFormat(eventCode) && this.codeExists(eventCode)) {
             console.log('Joining Event: ' +eventCode);
             this.context.router.transitionTo('pay', {eventCode: eventCode});
         }
@@ -45,7 +63,7 @@ var Join = React.createClass({
                 <TextField
                     ref = "eventCode"
                     errorText={this.state.errorText}
-                    onKeyUp={this.onKeyDown}
+                    onKeyUp={this.onKeyUp}
                     className = "upperCase"
                     floatingLabelText="Event Code"/>
                 <br/>
