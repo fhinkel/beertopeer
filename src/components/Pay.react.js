12e7293b45add45955d4a06abd0c72f6a0fa61b4
Steffen Rath <steffen.rath@tngtech.com>
2015-06-13 18:36:15 +0200

    added transition from payment screen to show screen

--- src/components/Pay.react.js
+++ src/components/Pay.react.js
@@ -22,6 +22,16 @@ var LoadingState = keyMirror({
     LOADED: null
 });
 
+var ErrorMessage = React.createClass({
+    render: function() {
+        if(this.props.message) {
+            return (<p><b>{this.props.message}</b></p>);
+        } else {
+            return (<div></div>);
+        }
+    }
+});
+
 var Pay = React.createClass({
     getInitialState: function() {
         return {
@@ -36,10 +46,15 @@ var Pay = React.createClass({
 
         var rippleAmount = ripple.Amount.from_human(amount + ' ' + this.state.currency);
 
+        this.setState({loadingState: LoadingState.LOADING});
         RippleService.pay(user.rippleSecret, this.state.targetRippleAccountId, rippleAmount, this.props.params.eventCode, function (success) {
             console.log('payment result ' + success);
-            // TODO
-        });
+            if(!success) {
+                this.context.router.transitionTo('pay', {eventCode: this.props.eventCode, errorMessage: "Payment failed! Try again?"});
+            } else {
+                this.context.router.transitionTo('show', {eventCode: this.props.eventCode});
+            }
+        }.bind(this));
 
     },
 
@@ -68,6 +83,7 @@ var Pay = React.createClass({
                 <div>
                     <h1>Contribute to event {this.state.eventname}</h1>
 
+                    <ErrorMessage message={this.props.errorMessage}/>
                     <p>This event has been created by {this.state.eventCreator}. The total requested amount
                         is {this.state.totalAmount} {this.state.currency}.</p>
                     <table>
@@ -76,11 +92,15 @@ var Pay = React.createClass({
                             <td>{this.state.currency}</td>
                         </tr>
                     </table>
-                    <RaisedButton label="Pay!" primary={true} onClick={this.onClickPayButton}/>
+                    <RaisedButton ref="payButton" label="Pay!" primary={true} onClick={this.onClickPayButton}/>
                 </div>
             );
         }
     }
 });
+
+Pay.contextTypes = {
+    router: React.PropTypes.func
+};
 
 module.exports = Pay;
