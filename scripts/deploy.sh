482aefdc0f5269470720be59f2d6eb3bd8344055
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2015-08-28 23:03:13 +0200

    fooooo force

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,27 +1,27 @@
-    #!/bin/bash
-    # Any command that using GH_OAUTH_TOKEN must pipe the output to /dev/null to not expose your oauth token
-    echo 'submodule'
-    echo ${GH_OWNER}
-    pwd
-    
+#!/bin/bash
+# Any command that using GH_OAUTH_TOKEN must pipe the output to /dev/null to not expose your oauth token
+echo 'submodule'
+echo ${GH_OWNER}
+pwd
+
 ls
-    git submodule add -b gh-pages https://${GH_OAUTH_TOKEN}@github.com/${GH_OWNER}/${GH_PROJECT_NAME} site 
-    ls
+git submodule add --force -b gh-pages https://${GH_OAUTH_TOKEN}@github.com/${GH_OWNER}/${GH_PROJECT_NAME} site 
+ls
 
-     cd site
-     echo 'checkout'
-     if git checkout gh-pages; then git checkout -b gh-pages; fi
-     git rm -r .
-     cp -R ../dist/* .
-     cp ../dist/.* .
-     echo 'add'
-     git add -f .
-     echo 'ls'
-     ls -al
+ cd site
+ echo 'checkout'
+ if git checkout gh-pages; then git checkout -b gh-pages; fi
+ git rm -r .
+ cp -R ../dist/* .
+ cp ../dist/.* .
+ echo 'add'
+ git add -f .
+ echo 'ls'
+ ls -al
 
-     git config user.email "franziska.hinkelmann@gmail.com"
-     git config user.name "Franziska Hinkelmann"
-     echo 'commit'
-     git commit -am "Deploy to gh-pages [ci skip]"
-    # Any command that using GH_OAUTH_TOKEN must pipe the output to /dev/null to not expose your oauth token
-     git push https://${GH_OAUTH_TOKEN}@github.com/${GH_OWNER}/${GH_PROJECT_NAME} HEAD:gh-pages 
+ git config user.email "franziska.hinkelmann@gmail.com"
+ git config user.name "Franziska Hinkelmann"
+ echo 'commit'
+ git commit -am "Deploy to gh-pages [ci skip]"
+# Any command that using GH_OAUTH_TOKEN must pipe the output to /dev/null to not expose your oauth token
+ git push https://${GH_OAUTH_TOKEN}@github.com/${GH_OWNER}/${GH_PROJECT_NAME} HEAD:gh-pages 
