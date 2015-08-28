439ede137a5618040c5612803b0f1f7ed5e12db9
Konstantin Knauf <konstantin.knauf@tngtech.com>
2015-08-28 21:27:54 +0200

    Debug info

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
     #!/bin/bash
     # Any command that using GH_OAUTH_TOKEN must pipe the output to /dev/null to not expose your oauth token
     echo 'submodule'
+    echo ${GH_OWNER}
     git submodule add -b gh-pages https://${GH_OAUTH_TOKEN}@github.com/${GH_OWNER}/${GH_PROJECT_NAME} site > /dev/null 2>&1
      cd site
      echo 'checkout'
