5e9df8a29d135d2188b9b162431f33722445eed7
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2015-08-28 22:32:05 +0200

    fooo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,11 @@
     # Any command that using GH_OAUTH_TOKEN must pipe the output to /dev/null to not expose your oauth token
     echo 'submodule'
     echo ${GH_OWNER}
+ls
     git submodule add -b gh-pages https://${GH_OAUTH_TOKEN}@github.com/${GH_OWNER}/${GH_PROJECT_NAME} site 
-     cd site
+    ls
+
+     cd scripts/site
      echo 'checkout'
      if git checkout gh-pages; then git checkout -b gh-pages; fi
      git rm -r .
